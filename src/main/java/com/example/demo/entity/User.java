9a14c41147d80bb8cc47a788e87d831711348724
yumotingfeng <1115179234@qq.com>
2019-12-05 11:15:46 +0800

    年龄加入不为空验证

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -15,6 +15,7 @@ public class User {
     private Integer id;
     @NotNull(message = "name用户名称不能为空")
     private String name;
+    @NotNull(message = "age年龄不能为空")
     private Integer age;
     private Integer leave;
 }
